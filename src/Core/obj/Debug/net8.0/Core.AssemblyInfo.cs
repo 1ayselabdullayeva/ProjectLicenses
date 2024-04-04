e03ef59ed70f22958913bede309c74907c0e7c22
1abdllyva <ayselabdullayeva657@gmail.com>
2024-04-04 13:13:23 +0400

    newChanges

--- src/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ src/Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c698a65b5e2bd1c8a081c9841d42340bede575")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3b1faecdd1c4dae7de9e9786a08fe01c65e2514")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
